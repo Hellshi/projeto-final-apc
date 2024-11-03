b143435db36095a07babdb024f8f210be660bbfd
hellshi <hcslimaa@gmail.com>
2024-11-03 19:33:17 -0300

    feat: adds loan and return

--- emprestimo.py
+++ emprestimo.py
@@ -1,3 +1,22 @@
+from gerenciador_de_arquivos import GerenciadorDeArquivos
+
+
 class Emprestimo:
     def __init__(self):
-        pass
+        self.estoque = GerenciadorDeArquivos('estoque/estoque.csv')
+
+    def emprestar(self):
+        cpf = input('informe o CPF do solicitante: ')
+
+        isbn = input('informe o ISBN do livro: ')
+
+        quantidade = int(input('informe a quantidade de livros: '))
+
+        self.estoque.atualizar_livro(isbn, 'quantidade', self.estoque.buscar_em_arquivo('isbn', isbn)['quantidade'] - quantidade)
+    
+    def devolver(self):
+        isbn = input('informe o ISBN do livro: ')
+
+        quantidade = int(input('informe a quantidade de livros: '))
+
+        self.estoque.atualizar_livro(isbn, 'quantidade', self.estoque.buscar_em_arquivo('isbn', isbn)['quantidade'] + quantidade)
